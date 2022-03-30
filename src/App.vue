d35b74d7426da2230ac206993526540a11b21f02
yar0316 <yarecon0316@gmail.com>
2022-03-31 00:14:59 +0900

    ready to fix mistake

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <v-toolbar elevation="4"></v-toolbar>
+    <v-container>
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+      <router-view/>
+    </v-container>
   </div>
 </template>
 
